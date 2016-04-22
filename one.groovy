284ab1e07a825cc0878e5f41e667d7ea57ef27ea
Alex Munoz <alex@napend.com>
2016-04-22 16:21:59 -0400

    Initial

---
+++ one.groovy
@@ -0,0 +1,30 @@
+class Learning{
+  static void main(String[] args){
+
+    // Print out text
+    println("Hello World");
+
+    // Variables
+    def age = "Dog";
+    age = 40;
+
+    // Math Functions
+    println("5 + 4 = " + (5 + 3));
+    println("5 - 4 = " + (5 - 3));
+    println("5 * 4 = " + (5 * 3));
+    println("5 / 4 = " + (5.intdiv(4)));
+    println("5 % 4 = " + (5 % 3));
+
+    println("5.2 + 4.4 = " + (5.2.plus(4.4)));
+    println("5.2 - 4.4 = " + (5.2.minus(4.4)));
+    println("5.2 * 4.4 = " + (5.2.multiply(4.4)));
+    println("5.2 / 4.4 = " + (5.2 / 4.4));
+
+    println("3 + 2 * 5 = " + (2 + 2 * 5));
+    println("(3 + 2) * 5 = " + ((3 + 2) * 5));
+
+    // Adding values to variables
+    println("age")
+
+  }
+}
